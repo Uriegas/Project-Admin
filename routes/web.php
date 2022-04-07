4ae165d11332053e39bf16ec3f2653430fd45f13
daniela-huerta <56332709+daniela-huerta@users.noreply.github.com>
2022-04-07 13:59:38 -0500

    Rutas de vistas de finanzas

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
-|--------------------------------------------------------------------------
+|------------------------------------------------------------------d--------
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
@@ -16,3 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/presupuesto', function () {
+    return view('Finanzas/presupuesto');
+});
+
+Route::get('/dashboard', function () {
+    return view('Finanzas/dashboardFinanzas');
+});
+
+Route::get('/reporte', function () {
+    return view('Finanzas/reporte');
+});
+
+Route::get('/cotizacion', function () {
+    return view('Finanzas/cotizacion');
+});
+
+Route::get('/visualizar', function () {
+    return view('Finanzas/visualizar_proyecto');
+});
+
