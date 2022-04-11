2d71863e42511b62b4ab7866d24cb8896d262073
1930080_upv <1930080@upv.edu.mx>
2022-04-11 15:27:15 -0500

    version1

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('Desarrollo', function () {
+    return view('Desarrollo/welcome');
+});
+
+Route::get('Desarrollo/tarjetas', function () {
+    return view('Desarrollo/tarjetas');
 });
