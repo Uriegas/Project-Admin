de504ac68df3648c155ab1dbf2576bb7120a94e3
Fernanda Coronado <56330513+fernandaCoronado@users.noreply.github.com>
2022-04-10 21:09:43 -0500

    Actualización del index de mrk-ventas

--- resources/views/mrk-ventas/index.blade.php
+++ resources/views/mrk-ventas/index.blade.php
@@ -1,35 +1,57 @@
 @extends('layouts.app')
 
+<style>
+    .card-hover:hover {
+        opacity: 0.8;
+    }
+</style>
+
+<!-- Bootstrap css -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
 @section('content')
 
 <!-- CABECERA -->
-<div class="page-header d-xl-flex d-block">
+<div class="page-header d-xl-flex d-block ml-5">
     <div class="page-leftheader">
-        <h4 class="page-title">Dashboard</h4>
-    </div>
-    {{-- DESCOMENTAR DIV PARA MOSTRAR BOTON DE AGREGAR --}}
-    {{-- <div class="page-rightheader ml-md-auto">
-        <div class="align-items-end flex-wrap my-auto right-content breadcrumb-right">
-            <div class="btn-list">
-                <a href="#" class="btn btn-primary mr-3">
-                    Agregar cosa</a>
-            </div>
-        </div> --}}
+        <h4 class="page-title mt-5">Marketing y Ventas</h4>
     </div>
 </div>
 <!-- FIN CABECERA -->
 
 
-
-
 <!-- CONTENIDO -->
 <div class="row">
     <div class="col-xl-12 col-md-12 col-lg-12">
-        
-        {{-- 
-            AQUI VA EL CONTENIDO DE LA VISTA 
-        --}}
-    
+        <div class="text-center ml-5">
+            <div class="row">
+                <div class="col-5">
+                    <div class="card text-white bg-success mb-3 card-hover">
+                        <div class="card-body">
+                            <br><br><br><br><br>
+                            <i class="bi bi-palette-fill" style="font-size: 60px"></i>
+                            <br><br>
+                            <h2 class="card-title">Estrategias de publicidad</h2>
+                            <a href="/mrk-ventas/estrategias_publicidad/dashboard" class="stretched-link"></a>
+                            <br><br><br><br><br>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-5">
+                    <div class="card text-white bg-info mb-3 card-hover">
+                        <div class="card-body">
+                            <br><br><br><br><br>
+                            <i class="bi bi-people-fill" style="font-size: 60px"></i>
+                            <br><br>
+                            <h2 class="card-title">Clientes</h2>
+                            <a href="/mrk-ventas/clientes/dashboard" class="stretched-link"></a>
+                            <br><br><br><br><br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 <!-- FIN CONTENIDO -->
